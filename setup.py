bdcd7452279637396a85396b8c83b47de99a74fc
David Anderson <dvander@alliedmods.net>
2020-07-15 18:51:22 -0700

    Revert "Switched from using 'distutils' to 'setuptools' for installing AMBuild."

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 # vim: set ts=2 sw=2 tw=99 et: 
 import sys
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 try:
   import sqlite3
 except:
-  raise SystemError('py-sqlite3 must be installed')
-  
-amb_scripts = []
-if sys.platform != 'win32':
-  if sys.platform == 'darwin':
-    amb_scripts.append('scripts/ambuild_dsymutil_wrapper.sh')
-  else:
-    amb_scripts.append('scripts/ambuild_objcopy_wrapper.sh')
+  raise Exception('py-sqlite3 must be installed')
+
+scripts = [
+  'scripts/ambuild'
+]
+
+if sys.platform == 'win32':
+  scripts.append('scripts/ambuild.bat')
+elif sys.platform == 'darwin':
+  scripts.append('scripts/ambuild_dsymutil_wrapper.sh')
+else:
+  scripts.append('scripts/ambuild_objcopy_wrapper.sh')
 
 setup(
   name = 'AMBuild',
@@ -22,12 +26,23 @@ setup(
   author = 'David Anderson',
   author_email = 'dvander@alliedmods.net',
   url = 'http://www.alliedmods.net/ambuild',
-  packages = find_packages(),
-  entry_points = {
-    'console_scripts': [
-      'ambuild = ambuild2.run:cli_run'
-    ]
-  },
-  scripts=amb_scripts
+  packages = [
+    'ambuild',
+    'ambuild2',
+    'ambuild2.frontend',
+    'ambuild2.ipc',
+    'ambuild2.frontend.v2_0',
+    'ambuild2.frontend.v2_0.amb2',
+    'ambuild2.frontend.v2_0.base',
+    'ambuild2.frontend.v2_0.cpp',
+    'ambuild2.frontend.v2_0.vs',
+    'ambuild2.frontend.v2_1',
+    'ambuild2.frontend.v2_1.amb2',
+    'ambuild2.frontend.v2_1.base',
+    'ambuild2.frontend.v2_1.cpp',
+    'ambuild2.frontend.v2_1.vs',
+    'ambuild2.frontend.v2_1.tools',
+  ],
+  scripts = scripts
 )
 
